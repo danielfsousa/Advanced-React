f8ff32947b456aacbe6347e509116b621fa8d0c2
Daniel Sousa <sousa.dfs@gmail.com>
2019-06-13 12:21:02 -0300

    completes lesson 40

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -179,6 +179,35 @@ const mutations = {
       },
       info
     )
+  },
+
+  async addToCart (parent, args, ctx, info) {
+    const { userId } = ctx.request
+    if (!userId) {
+      throw new Error('You must be signed in')
+    }
+
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id }
+      }
+    })
+
+    if (existingCartItem) {
+      console.log('Thos item is already in their cart')
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 }
+      }, info)
+    }
+
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: { connect: { id: userId } },
+        item: { connect: { id: args.id } }
+      }
+    }, info)
   }
 }
 
